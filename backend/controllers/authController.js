42014da030aba81fb65bbd86552fce8e4c2f4cbc
Josh Andrei Bolante <joshandreibolante05@gmail.com>
2026-02-08 12:04:33 +0800

    jwt token in authCont

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ import { User } from "../models/userModel.js";
 import Student from "../models/studentModel.js";
 import fs from "fs";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import crypto from "crypto";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 import {
