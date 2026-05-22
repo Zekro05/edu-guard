0df758513868199ec6ce4657efa7871b1debf933
Josh Andrei Bolante <joshandreibolante05@gmail.com>
2026-05-22 09:35:31 +0800

    plsetonayon

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
